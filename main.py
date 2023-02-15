bffab7d8bc5ab2e41aa5c05ba116fdd35a583fb6
ajay <ajayy069@gmail.com>
2023-02-16 00:28:35 +0530

    first commit

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import final_front
-import final_back
+from aadhar_ocr import front, back
 import cv2
+import json
 
 
 
 front_img = cv2.imread(r'C:\Users\YADAV\OneDrive\Desktop\Aadhaar ocr\front_img.jpeg')
 back_img = cv2.imread(r'C:\Users\YADAV\OneDrive\Desktop\Aadhaar ocr\back_img.jpeg')
 
-front_det=final_front(front_img)
+front_det = front(front_img)
+back_det = back(back_img)
 
-back_det=final_back(back_img)
+print(front_det)
+print(back_det)
 
-import json
-json_string = json.dumps(front_det,back_det)
+aadhar_dict = front_det | back_det
+
+with open("extract.json","w") as outfile:
+    json.dump(aadhar_dict,outfile)
